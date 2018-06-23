d6f41add08e4b6ef31c858fe8f2d10bc3d23456c
Sean Pollock <seanpollock3344@gmail.com>
2018-06-23 06:15:20 -0400

    basic routine

--- routine.go
+++ routine.go
@@ -1,15 +1,23 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "time"
+  "math/rand"
+)
 
 func do(n int) {
   for i := 0; i < 10; i++ {
     fmt.Println(n, ":", i)
+    amt := time.Duration(rand.Intn(250))
+    time.Sleep(time.Millisecond * amt)
   }
 }
 
 func main() {
-  go do(0)
+  for i := 0; i < 10; i++ {
+    go do(0)
+  }
   var input string
   fmt.Scanln(&input)
 }
